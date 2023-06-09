6758e51b87ff2578f223171eb8ca7ba3845ae350
Pinchiri <rolando4693@gmail.com>
2023-06-09 16:55:16 -0400

    Added React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+//import App from './App.jsx'
 import './index.css'
+import { HomePage } from './pages/HomePage/HomePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<HomePage />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
