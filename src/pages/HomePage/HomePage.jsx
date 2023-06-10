8896406f8f0de9d25ac09f1712f29185bb7deb55
Tomás Gil <tommygilbravo@gmail.com>
2023-06-10 14:09:56 -0400

    Conexión con la API

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import{ MovieCard } from '../../components/MovieCard/MovieCard'
+import { fetchMovies } from '../../utils/movie-api'
 
 export function HomePage() {
+  const [movie, setMovie] = useState([])
+
+  const getMovies = async () =>{
+    const {data} = await fetchMovies()
+
+    console.log(data);
+  }
+
+  useEffect(()=>{
+    getMovies()
+  }, [])
+
   return (
     <div>
 
