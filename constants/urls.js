26cb613e9ae21780b453c478f652d82d453da2d2
Tomás Gil <tommygilbravo@gmail.com>
2023-06-10 11:37:49 -0400

    Footer

--- constants/urls.js
+++
@@ -1,6 +0,0 @@
-export const homeURL = "/"
-export const registerURL = "/register"
-export const loginURL = "/login"
-export const profileURL = "/profile"
-export const reserveURL = "/reserve"
-export const adminURL = "/admin"
--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,12 @@ import { Routes, Route, BrowserRouter } from "react-router-dom"
 //import App from './App.jsx'
 import './index.css'
 import { HomePage } from './pages/HomePage/HomePage.jsx'
-import { Layout } from './pages/Layout/Layout'
-import { homeURL, reserveURL } from '../constants/urls'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route element = {<Layout />}>
-        <Route path = {homeURL} element = {<HomePage />} />
-        <Route path = {reserveURL} element = {<div>Hola</div>} />
-      </Route>
-      
+      <Route path = "/" element = {<HomePage />} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
