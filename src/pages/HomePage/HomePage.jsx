c7d58b68696b00964e933774ee0aab6164d06fb7
Tomás Gil <tommygilbravo@gmail.com>
2023-06-10 20:13:01 -0400

    Progreso con la API

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,34 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import{ MovieCard } from '../../components/MovieCard/MovieCard'
+import { fetchMovies } from '../../utils/movie-api'
 import { fetchNewMovies } from '../../utils/movie-api'
 import styles from './HomePage.module.css';
 
 export function HomePage() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([])  
+  const [newMovies, setNewMovies] = useState([])
   const [isLoading, setLoading] = useState(false);
 
+
+  const getMovies = async () =>{
+    setLoading(true);
+    const {data} = await fetchMovies()
+    setMovies(data.results);
+    setLoading(false);
+    console.log(data.results);
+  }
 
   const getNewMovies = async () =>{
     setLoading(true);
     const {data} = await fetchNewMovies()
-    setMovies(data.results);
+    setNewMovies(data.results);
     setLoading(false);
+    console.log(data.results);
   }
 
   useEffect(()=>{
+    getMovies()
     getNewMovies()
   }, [])
 
@@ -38,6 +50,17 @@ export function HomePage() {
       
       <div className={styles.titleContainer}>      
         <h1 className={styles.title}>PRÓXIMOS ESTRENOS</h1>  
+      </div>
+
+      <div className={styles.movies}>
+        {isLoading && (
+          <h1>CARGANDO...</h1>
+        )}
+        {!isLoading && newMovies.map((movie)=>{
+          return(          
+            <MovieCard movie={movie} key={movie.id}/>
+          )
+        })}
       </div>  
       
     </div>    
